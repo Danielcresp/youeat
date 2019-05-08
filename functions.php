b4a48b18dbe6768e8156706c442236cc8849b958
Danielcresp <dneli6cres@outlook.com>
2019-05-07 20:48:01 -0600

    Crear menus dinamicos  y mostrarlos en paguinas

--- functions.php
+++ functions.php
@@ -2,12 +2,34 @@
 
 function youeat_styles(){
      //rigistrar stylos css
+    
+      //Registrar 
+    wp_register_style('normalize',get_template_directory_uri().'/css/normalize.css',array() ,'8.0');
+    wp_register_style('style',get_template_directory_uri().'style.css',array() ,'1.0'); //css registrar en wordpress
+    wp_enqueue_style('index',get_stylesheet_directory_uri().'/css/index.css');
+    //Obtener 
+    wp_enqueue_style('style');  
+    wp_enqueue_style('normalize');   
+    wp_enqueue_script('jquery'); 
+    wp_enqueue_style('index'); 
     wp_enqueue_style('normalize',get_stylesheet_directory_uri().'/css/normalize.css');
     wp_enqueue_script('jquery');
     wp_enqueue_style('bootstrap',"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
     wp_enqueue_script('bootstrapjs',"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",array('jquery'));
     wp_enqueue_style('style',get_stylesheet_uri());
-
 }
 
+//Menu
+function youeat_menus(){
+    register_nav_menus(array(
+        'header-menu' => __('Header Menu','youeat'),
+        'social-menu' => __('Social Menu','youeat'),
+    ));
+}
+
+
+
 add_action('wp_enqueue_scripts','youeat_styles');
+add_action('init','youeat_menus');
+
+
---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eat</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header class="encabezado_sitio">
+        <div class="contenedor">
+            <div class="logo">
+                 <a href="<?php echo esc_url(home_url('/')); ?>"> <!--//sanitizacion para seguridad -->
+                   <img class='logo_img' src="<?php echo get_template_directory_uri(); ?>/img/logo.png">
+                </a>
+            </div><!-- .logo -->
+            <div class="informacion-encabezado">
+                <div class="redes-sociales">
+                    <?php
+                    $args = array(
+                        'theme_location' => 'social-menu',
+                        'container' => 'nav',
+                        'container_class' => 'menu-social',
+                        'container_id' => 'menu-social',
+                        'link_before' => '<span class="sr-text">',
+                        'link_after' => '</span>'
+                    );
+                    wp_nav_menu($args);
+                    ?>
+                </div>
+            </div> <!-- .redes -->
+            <div class="direcciones">
+                <p>6345 Avenida de la Pizza CA 543435</p>
+                <p>Telefono: +1-342-533-5343</p>
+            </div>
+        </div><!-- .contenedor -->
+    </header>
+    <nav class="menu-sitio">
+        <div class="contenedor navegacion">
+            <?php 
+                $args = array(
+                    'theme_location' => 'header-menu',
+                    'container' => 'nav',
+                    'container_class' => 'menu-sitio'
+                );
+                wp_nav_menu( $args);
+                 //menu
+            ?>
+        </div>
+    </nav>
