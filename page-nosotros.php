9b9b735a837d882cdf1264a30fa290091e50ee71
Danielcresp <dneli6cres@outlook.com>
2019-05-16 22:15:15 -0600

    Teminar layout de Nosotros | Error responsive

--- page-nosotros.php
+++ page-nosotros.php
@@ -9,22 +9,47 @@
             </div>
         </div>
             
-        <div class="principal contenedor">
+        <div class="principal contenedor clear">
              <main class="text-centrado contenido-paguinas">
                 <?php the_content();?> <!-- Comentario -->
             </main>
         </div>    
-        <div class="informacion-caja contenedor">
-            <?php 
-            $id_imagen = get_field('imagen_1');
-            $imagen = wp_get_attachment_image_src($id_imagen,'nosotros');
-            ?>
-            <img src="<?php echo $imagen[0]; ?>" class="imagen-caja">
+        <!-- cajas  -->
+        <div class="info-cajas-main clear">
+            <div class="informacion-caja cont">
+                <?php 
+                $id_imagen = get_field('imagen_1');
+                $imagen = wp_get_attachment_image_src($id_imagen,'nosotros');
+                ?>
+                <img src="<?php echo $imagen[0]; ?>" class="imagen-caja">
 
-            <div class="contenido-caja">
-                <?php the_field('descripción_1');?>
+                <div class="contenido-caja  pin1">
+                    <?php the_field('descripción_1');?>
+                </div>
             </div>
+            <div class="informacion-caja cont">
+            <div class="contenido-caja pin2">
+                    <?php the_field('descripción_2');?>
+                </div>
+                <?php 
+                $id_imagen = get_field('imagen_2');
+                $imagen = wp_get_attachment_image_src($id_imagen,'nosotros');
+                ?>
+                <img src="<?php echo $imagen[0]; ?>" class="imagen-caja">
+            </div>
+            <div class="informacion-caja cont">
+                <?php 
+                $id_imagen = get_field('imagen_3');
+                $imagen = wp_get_attachment_image_src($id_imagen,'nosotros');
+                ?>
+                <img src="<?php echo $imagen[0]; ?>" class="imagen-caja">
+
+                <div class="contenido-caja pin3">
+                    <?php the_field('descripción_3');?>
+                </div>
+            </div>  
         </div>
     <?php endwhile; ?>
 
 <?php get_footer(); ?>
+
