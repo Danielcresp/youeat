5b9297d3ea910a50a399f1fd549d0bcb2b30e30a
Danielcresp <dneli6cres@outlook.com>
2019-05-17 23:02:34 -0600

    Imprimir Entradas del Blog añadir nuevas entradas

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
 <?php get_header(); ?>
+<?php  $pagina_blog = get_option('page_for_posts');
+        $imagen = get_post_thumbnail_id($pagina_blog); //id de la imagen
+        $imagen = wp_get_attachment_image_src($imagen,'full');
+?> 
 
-<?php echo  "Primer Thema en wordpress de YOUEAT INDEX.PHP" ?>
+        <div class="hero" style="background-image:url(<?php echo  $imagen[0]; ?>);">
+            <div class="contenido-hero">
+                <div class="texto-hero">
+                <h1><?php echo get_the_title($pagina_blog); ?></h1> <!-- Imprimir Titulo -->
+                </div>
+            </div>
+        </div>
+            
+        <div class="principal contenedor">
+             <main class="text-centrado contenido-paguinas">
+                <?php  while(have_posts()): the_post();?> <!-- Recorrer informacio -->
+                    <article class="entrada-blog">
+                        <a href="<?php the_permalink();?>">
+                            <?php the_post_thumbnail('especialidades');?>
+                        </a>
+                        <header class="informacion-entrada clear">
+                            <div class="fecha">
+                                <time>
+                                    <?php echo the_time('d'); ?>
+                                    <span><?php the_time('M'); ?></span>
+                                </time>
+                            </div>
+                            <div class="titulo-entrada">
+                                <?php the_title('<h1>','</h1>'); ?>
+                                <p class="autor">
+                                    <i class="fad fa-user" aria-hidden='true'></i>
+                                    <?php the_author();?>
+                                </p>
+                            </div>
+                        </header>
+                        <div class="contenido-entrada">
+                            <?php the_excerpt();?>
+                            <a href="<?php the_permalink();?>" class="button rojo">Leer Más</a>
+                        </div>
+                    </article>
+                     <?php the_title();?>
+                <?php endwhile; ?>
+            </main>
+        </div>    
+
 
 <?php get_footer(); ?>
