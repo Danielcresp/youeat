d801ce28d4e3fc268dfdb06effbda1c5ee3cb844
Danielcresp <dneli6cres@outlook.com>
2019-05-30 23:49:46 -0600

    Template de  inicio e imprimir Slogan de sitio

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+    <?php  while(have_posts()): the_post();?> <!-- Recorrer informacio -->
+        <div class="hero" style="background-image:url(<?php echo  get_the_post_thumbnail_url(); ?>);">
+            <div class="contenido-hero">
+                <div class="texto-hero">
+                <?php echo esc_html(get_option('blogdescription')); ?>
+                <?php the_content();?> <!-- Comentario -->
+                <?php $url =get_page_by_title('Sobre Nosotros')?>
+                <a class="button rojo" href="<?php echo get_permalink($url->ID) ?>">Leer mas</a>
+                </div>
+            </div>
+        </div>
+            
+        <div class="principal contenedor">
+             <main class="text-centrado contenido-paguinas">
+            </main>
+        </div>    
+    <?php endwhile; ?>
+
+<?php get_footer(); ?>
